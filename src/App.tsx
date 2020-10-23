1eb93da1471057daa7968d43ac05cc4f5560312f
ST <shaotian.li.april@gmail.com>
2020-10-23 19:36:30 +0800

    创建 views 目录

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,16 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  // eslint-disable-next-line
-  Link,
   Redirect
 } from "react-router-dom";
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-const Wrapper = styled.div`
-height: 100vh;
-display:flex;
-flex-direction: column;
-
-`
-const Main = styled.div`
-flex-grow: 1;
-overflow: auto;
-`
+import Tags from './views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
       <Router>
-        <Wrapper>
-          <Main>
           <Switch>
             <Route path="/tags">
               <Tags />
@@ -41,29 +28,11 @@ function App() {
               <NoMatch />
             </Route>
           </Switch>
-          </Main>
-          <Nav />
-        </Wrapper>
       </Router>
   );
 }
 
-function NoMatch(){
-  return(
-      <div>Oops！页面不存在，地址可能输错啦～ovo</div>
-  )
-}
 
-function Statistics() {
-  return <h2>统计页面</h2>;
-}
 
-function Tags() {
-  return <h2>标签页面</h2>;
-}
-
-function Money() {
-  return <h2>记账页面</h2>;
-}
 
 export default App;
