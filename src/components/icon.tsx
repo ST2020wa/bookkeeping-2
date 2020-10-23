a2ea749a4578a07a6fc1c7a083f2fb6153415951
ST <shaotian.li.april@gmail.com>
2020-10-23 17:09:35 +0800

    创建 Icon 组件

---
+++ src/components/icon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+let importAll = (requireContext: __WebpackModuleApi.RequireContext ) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../icons', true, /\.svg$/));} catch(error){console.log(error);}
+
+//  require 一个目录/文件夹
+
+type Props = {
+    name: string
+}
+
+const Icon = (props: Props)=>{
+    return (
+        <svg className="icon">
+            <use xlinkHref={'#' + props.name}/>
+        </svg>
+    )
+};
+
+export default Icon;
