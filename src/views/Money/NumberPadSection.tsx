493f4b4e767b84033b448d63035afa3e65bb2022
ST <shaotian.li.april@gmail.com>
2020-10-26 13:16:33 +0800

    NumberPadSection WIP

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const NumberPadSection = styled.section `
+const Wrapper = styled.section `
   display: flex;
   flex-direction: column;
   >.output{
@@ -57,5 +58,69 @@ const NumberPadSection = styled.section `
     }
   }
 `;
+
+const NumberPadSection: React.FC = ()=> {
+    const [output, setOutput] = useState('0');
+    const onClickButtonWrapper = (e: React.MouseEvent) => {
+        const text = (e.target as HTMLButtonElement).textContent;
+        if(text === null) {return;}
+        if(text){
+            switch(text) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '.':
+                    if(output === '0'){
+                        setOutput(text);
+                    }else{
+                        setOutput(output + text);
+                    }
+                    break;
+                case 'Backspace':
+                    console.log('Backspace');
+                    break;
+                case 'Clear':
+                    console.log('Clear');
+                    break;
+                case '.':
+                    console.log('.');
+                    break;
+                case 'OK':
+                    console.log('OK');
+                    break;
+            }
+        }
+    };
+    return (
+        <Wrapper>
+            <div className="output">
+                {output}
+            </div>
+            <div className="pad clearfix" onClick={onClickButtonWrapper}>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>Backspace</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>Clear</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button className="ok">OK</button>
+                <button className="zero">0</button>
+                <button>.</button>
+            </div>
+        </Wrapper>
+    )
+}
 
 export {NumberPadSection};
